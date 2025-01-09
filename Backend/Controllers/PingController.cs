3bbc79cc1c452a917b1270eba01dc1a1d082b626
Skalersaas <efvhdg@gmail.com>
2025-01-09 18:24:57 +0400

    x

---
+++ Backend/Controllers/PingController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Anonymous_Chat.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class PingController : ControllerBase
+    {
+        [HttpGet]
+        public ObjectResult Ping()
+        {
+            return Ok("Working");
+        }
+    }
+}
