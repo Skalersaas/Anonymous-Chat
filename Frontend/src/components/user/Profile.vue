605dcce9354718e983bce12fd58cb04ad7c768c8
Skalersaas <125687364+Skalersaas@users.noreply.github.com>
2025-01-09 23:26:17 +0400

    Update Profile.vue

--- Frontend/src/components/user/Profile.vue
+++ Frontend/src/components/user/Profile.vue
@@ -58,7 +58,7 @@ export { profile };
 		  class="block w-full rounded-lg border border-gray-300 py-2 px-4 shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-400 sm:text-sm"
 		  required
 		>
-		  <option :value="" disabled selected="true">Select your gender</option>
+		  <option value="" disabled selected="true">Select your gender</option>
 		  <option :value="genders.male">Male</option>
 		  <option :value="genders.female">Female</option>
 		  <option :value="genders.other">Other</option>
